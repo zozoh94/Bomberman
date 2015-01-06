9ad2c75bb38b457bf7d75a8a00dea4d06783c7dd
Enzo Hamelin <enzo.hamelin@gmail.com>
2015-01-06 15:18:23 +0100

    Initialisation de la SDL

---
+++ src/bomberman.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+#include "SDL/SDL.h"
+
+void pause();
+
+int main(int argc, char **argv)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) == -1) // Démarrage de la SDL. Si erreur :
+    {
+        fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError()); // Écriture de l'erreur
+        exit(EXIT_FAILURE); // On quitte le programme
+    }
+
+    SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE); // Ouverture de la fenêtre
+    
+    pause(); // Mise en pause du programme
+    
+    SDL_Quit();
+    
+    return EXIT_SUCCESS;
+}
+
+void pause()
+{
+    int continuer = 1;
+    SDL_Event event;
+ 
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+        }
+    }
+}
